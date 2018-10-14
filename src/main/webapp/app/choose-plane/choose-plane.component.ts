dd39bfd7eac7c40cd2a4b5092e228633910100f4
eduardodsaraujo <eduardodsaraujo@hotmail.com>
2018-10-14 17:45:07 -0300

    Efeito flip adicionado a Choose Plane component

--- src/main/webapp/app/choose-plane/choose-plane.component.ts
+++ src/main/webapp/app/choose-plane/choose-plane.component.ts
@@ -1,21 +1,41 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
-  selector: 'jhi-choose-plane',
-  templateUrl: './choose-plane.component.html',
-  styleUrls: [
-    'choose-plane.css'
-  ]
+    selector: 'jhi-choose-plane',
+    templateUrl: './choose-plane.component.html',
+    styleUrls: ['choose-plane.css'],
+    animations: [
+        trigger('flipState', [
+            state(
+                'active',
+                style({
+                    transform: 'rotateY(179.9deg)'
+                })
+            ),
+            state(
+                'inactive',
+                style({
+                    transform: 'rotateY(0)'
+                })
+            ),
+            transition('active => inactive', animate('500ms ease-out')),
+            transition('inactive => active', animate('500ms ease-in'))
+        ])
+    ]
 })
 export class ChoosePlaneComponent implements OnInit {
+    message: string;
+    flip = 'inactive';
 
-  message: string;
+    constructor() {
+        this.message = 'ChoosePlaneComponent message';
+    }
 
-  constructor() {
-    this.message = 'ChoosePlaneComponent message';
-  }
+    ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+    // Troca o estado de flip, acionando a animacao
+    toggleFlip() {
+        this.flip = this.flip === 'inactive' ? 'active' : 'inactive';
+    }
 }
