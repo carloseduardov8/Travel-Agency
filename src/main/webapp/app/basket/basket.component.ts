620c962beb6accab4306742c040d4b3b59e5837a
eduardodsaraujo <eduardodsaraujo@hotmail.com>
2018-10-14 15:05:09 -0300

    Adicionado efeito de flip

--- src/main/webapp/app/basket/basket.component.ts
+++ src/main/webapp/app/basket/basket.component.ts
@@ -1,13 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
     selector: 'jhi-basket',
     templateUrl: './basket.component.html',
-    styleUrls: ['basket.css']
+    styleUrls: ['basket.css'],
+    animations: [
+        trigger('flipState', [
+            state(
+                'active',
+                style({
+                    transform: 'rotateY(179.9deg)'
+                })
+            ),
+            state(
+                'inactive',
+                style({
+                    transform: 'rotateY(0)'
+                })
+            ),
+            transition('active => inactive', animate('500ms ease-out')),
+            transition('inactive => active', animate('500ms ease-in'))
+        ])
+    ]
 })
 export class BasketComponent implements OnInit {
     message: string;
+    flip = 'inactive';
 
     constructor(private route: ActivatedRoute, private router: Router) {
         this.route.params.subscribe(params => {
@@ -22,5 +42,10 @@ export class BasketComponent implements OnInit {
 
     doSearch(query) {
         console.log(query);
+    }
+
+    // Troca o estado de flip, acionando a animacao
+    toggleFlip() {
+        this.flip = this.flip === 'inactive' ? 'active' : 'inactive';
     }
 }
