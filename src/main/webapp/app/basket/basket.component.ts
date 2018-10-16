ceabc092a393481b90aa493658e8646e5f6f03e4
eduardodsaraujo <eduardodsaraujo@hotmail.com>
2018-10-16 08:06:36 -0300

    Comecando filtrovoos

--- src/main/webapp/app/basket/basket.component.ts
+++ src/main/webapp/app/basket/basket.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import {VooService} from "app/entities/voo";
+import {HttpErrorResponse, HttpResponse} from "@angular/common/http";
+import {IVoo} from "app/shared/model/voo.model";
 
 @Component({
     selector: 'jhi-basket',
@@ -9,14 +12,25 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class BasketComponent implements OnInit {
     message: string;
 
-    constructor(private route: ActivatedRoute, private router: Router) {
+    constructor(private route: ActivatedRoute, private router: Router, private vooService:VooService) {
         this.route.params.subscribe(params => {
             console.log(params);
             if (params['query']) {
                 this.doSearch(params['query']);
             }
         });
+
+        console.log("ok");
+
+        console.log(this.vooService.findVoos("20-10-2018 15:00", "1", "2").subscribe(
+            res => {
+                console.log(res);
+            },
+            (res) => (console.log(res.message))
+        ));
     }
+
+
 
     ngOnInit() {}
 
