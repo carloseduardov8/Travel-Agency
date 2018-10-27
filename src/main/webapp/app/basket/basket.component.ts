a2e6e684a7ad5093fce62076c7d40924bed1d1ef
eduardodsaraujo <eduardodsaraujo@hotmail.com>
2018-10-27 12:23:59 -0200

    Home e menu ok

---
+++ src/main/webapp/app/basket/basket.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'jhi-basket',
+    templateUrl: './basket.component.html',
+    styles: []
+})
+export class BasketComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -3,9 +3,19 @@ import { RouterModule } from '@angular/router';
 
 import { ViajatoSharedModule } from 'app/shared';
 import { HOME_ROUTE, HomeComponent } from './';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-    imports: [ViajatoSharedModule, RouterModule.forChild([HOME_ROUTE])],
+    imports: [
+        ViajatoSharedModule,
+        BrowserModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        RouterModule.forChild([HOME_ROUTE])
+    ],
     declarations: [HomeComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
