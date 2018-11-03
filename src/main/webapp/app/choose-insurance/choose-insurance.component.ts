9aa00aab91c6cd585800fdd6deee6489bd7336f8
eduardodsaraujo <eduardodsaraujo@hotmail.com>
2018-11-03 20:34:03 -0200

    Ajuste preco seguros

--- src/main/webapp/app/choose-insurance/choose-insurance.component.ts
+++ src/main/webapp/app/choose-insurance/choose-insurance.component.ts
@@ -7,6 +7,10 @@ import { IVoo } from 'app/shared/model/voo.model';
 import { SeguroService } from 'app/entities/seguro';
 import { ISeguro, Seguro } from 'app/shared/model/seguro.model';
 import { Location } from '@angular/common';
+import { Contrato } from 'app/shared/model/contrato.model';
+import * as moment from 'moment';
+import _date = moment.unitOfTime._date;
+import { BasketService } from 'app/basket/basket.service';
 
 @Component({
     selector: 'jhi-choose-insurance',
@@ -19,13 +23,16 @@ export class ChooseInsuranceComponent implements OnInit {
     withoutSeguros: boolean;
     passengers = 1;
     diffDays = 1;
+    dateIn: Date;
+    dateOut: Date;
 
     constructor(
         private route: ActivatedRoute,
         private router: Router,
         private seguroService: SeguroService,
         private jhiAlertService: JhiAlertService,
-        private location: Location
+        private location: Location,
+        private basketService: BasketService
     ) {
         this.route.params.subscribe(params => {
             console.log(params);
@@ -36,10 +43,29 @@ export class ChooseInsuranceComponent implements OnInit {
 
                     // Preco calculado pelo preco * numero de passageiros * num de dias
                     if (params.dateOut) {
-                        let dateIn = new Date(params.dateIn);
-                        let dateOut = new Date(params.dateOut);
-                        let diff = Math.ceil(Math.abs(dateOut.getTime() - dateIn.getTime()) / (1000 * 3600 * 24));
+                        this.dateIn = new Date(
+                            params.dateIn
+                                .split('-')
+                                .reverse()
+                                .join('/')
+                        );
+                        this.dateOut = new Date(
+                            params.dateOut
+                                .split('-')
+                                .reverse()
+                                .join('/')
+                        );
+                        let diff = Math.ceil(Math.abs(this.dateOut.getTime() - this.dateIn.getTime()) / (1000 * 3600 * 24));
+                        console.log('Diff ' + diff);
                         this.diffDays = diff > 1 ? diff : this.diffDays;
+                    } else {
+                        this.dateIn = new Date(
+                            params.dateIn
+                                .split('-')
+                                .reverse()
+                                .join('/')
+                        );
+                        this.dateOut = this.dateIn;
                     }
 
                     this.seguroService.findSeguros(params.to).subscribe((res: HttpResponse<IVoo[]>) => {
@@ -58,6 +84,18 @@ export class ChooseInsuranceComponent implements OnInit {
     // Comprar seguro
     buy(insurance) {
         console.log(insurance);
+        let contrato = new Contrato();
+        contrato.seguro = insurance;
+        contrato.dataInicio =
+            ('0' + this.dateIn.getDate()).slice(-2) + '-' + (this.dateIn.getMonth() + 1) + '-' + this.dateIn.getFullYear() + ' 00:00:00';
+
+        contrato.dataFim =
+            ('0' + this.dateOut.getDate()).slice(-2) + '-' + (this.dateOut.getMonth() + 1) + '-' + this.dateOut.getFullYear() + ' 00:00:00';
+
+        contrato.numPessoas = this.passengers;
+        contrato.compra = this.basketService.compra;
+
+        console.log(contrato);
     }
 
     // Funcao para voltar ao componente anterior
